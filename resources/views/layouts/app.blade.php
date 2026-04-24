5eb4acf54c0910b83b9b08859ec3bc1906335b33
Brayan Tasayco <brayantasaycohuaman@gmail.com>
2026-04-24 09:07:51 -0500

    Actualizacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,8 @@
     <title>@yield('title', 'Dashboard')</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-
-    @stack('styles')
 </head>
 
 <body>
@@ -22,21 +18,16 @@
     <div class="container-fluid" style="padding-top: 76px;">
         <div class="row">
 
-            <!-- Sidebar -->
-            <div class="col-lg-2 d-none d-lg-block">
-                @if(!View::hasSection('noSidebar'))
+            <!-- SIDEBAR -->
+            @if(!View::hasSection('noSidebar'))
+                <div class="col-lg-2 d-none d-lg-block">
                     @include('components.sidebar')
-                @endif
-            </div>
+                </div>
+            @endif
 
-            <!-- Contenido -->
-            <div class="col-12 col-lg-7 p-4">
+            <!-- CONTENIDO -->
+            <div class="@if(View::hasSection('noSidebar')) col-12 @else col-12 col-lg-10 @endif p-4">
                 @yield('content')
-            </div>
-
-            <!-- Right Sidebar -->
-            <div class="col-xl-3 d-none d-xl-block">
-                @include('components.right-sidebar')
             </div>
 
         </div>
@@ -46,7 +37,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    @stack('scripts')
 </body>
 
 </html>
